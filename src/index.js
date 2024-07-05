5972461168698a12b620d1892b857bf60e81376a
rts-tamak <RiteshTamak@outlook.com>
2024-07-05 12:41:41 +0530

    cloudinary setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js"
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import express from "express";
 
 dotenv.config({
     path: "./env"
 })
-
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
